d70ca03aacb34853bef4d0165e5804cd3b281c1e
Felipe Baz <fbazmitsuishi@gmail.com>
2023-03-30 15:53:26 -0300

    :package:  composition

---
+++ composition/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	type text struct {
+		Name        string
+		words		int
+	}
+
+	type book struct {
+		text
+		Name        string
+		Published   bool
+		Author      string
+		releaseDate string
+	}
+
+	pacman := book{
+		text: text{Name: "pac-man", words: 1012831683761236712},
+		Published:   true,
+		Author:      "Shigeo Funaki Shigeichi Ishimura",
+		releaseDate: "July 1980",
+	}
+
+
+
+	fmt.Printf("Pacman: %+v\n", pacman)
+}
